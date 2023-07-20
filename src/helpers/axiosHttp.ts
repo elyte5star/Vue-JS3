f88d283352841f7bd45111636d56b9e773bc5442
elyte5star <checkuti@gmail.com>
2023-07-20 21:01:33 +0200

    fixed axios instance

--- src/helpers/axiosHttp.ts
+++ src/helpers/axiosHttp.ts
@@ -40,6 +40,13 @@ axiosInstance.interceptors.response.use(function (response) {
     const { user, logout } = userAuthStore()
     if ([401, 403].includes(error.response.status) && user) {
 
+        //         Swal.fire({
+        //             icon: 'error',
+        //             title: 'Oops...',
+        //             text: "Session Expired",
+        //             footer: '<a href="/login">Please, log in again!.</a>'
+        //         })
+
         return logout();
 
     }
@@ -47,38 +54,5 @@ axiosInstance.interceptors.response.use(function (response) {
     return Promise.reject(error);
 });
 
-
-
-
-
-
-
-
-function handleResponse(response: any) {
-    console.log(response)
-
-    // const isJson = response.headers?.get('content-type')?.includes('application/json');
-    // const data = isJson ? await response.json() : null;
-    // // check for error response
-
-    // if (!response.ok) {
-    //     const { user, logout } = userAuthStore();
-    //     if ([401, 403].includes(response.status) && user) {
-    //         // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-    //         Swal.fire({
-    //             icon: 'error',
-    //             title: 'Oops...',
-    //             text: "Session Expired",
-    //             footer: '<a href="/login">Please, log in again!.</a>'
-    //         })
-    //         logout();
-    //     }
-    //     // get error message from body or default to response status
-    //     const error = (data && data.message) || response.status;
-    //     return Promise.reject(error);
-    // }
-
-    // return data;
-}
 
 
